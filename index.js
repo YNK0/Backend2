5085946043b8535d73d5d0e7d97996c6cf6de601
YNK0 <francisco.vargasmagana03@gmail.com>
2024-11-07 17:38:34 -0600

    Add initial HTML, CSS, and SVG assets for the project

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-import express from 'express';
-import cors from 'cors';
+const express = require('express');
+const cors = require('cors');
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static('dist'));
 
 const requestLogger = (req, _res, next) => {
     console.log('Method:', req.method);
